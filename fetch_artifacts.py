d96ac3ac779f9ac1a79037736bb8100aa886b331
Navaneethan <nava071@gmail.com>
2020-10-23 12:49:21 +0530

    Removed incomplete slack integration code

--- fetch_artifacts.py
+++ fetch_artifacts.py
@@ -1,5 +1,3 @@
-# This is a handy script for getting the build files names from the artifactory after
-# we built from jenkins.
 
 import sys
 import json
@@ -8,14 +6,10 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-# 'items.find({"repo":{"$match":"'+ repo_name +'"},"name":{"$match":"'+ filenamepattern +'"},"created":{
-# "$last":"'+lastbuildstartdaysbefore+'"}}).include("name","path","repo","created","actual_sha1","original_sha1")'
-
 
 def get_input_from_user():
-    global display
+
     repo_name = input("Enter the repo (release, master, prod):")
-    display += repo_name
     lastbuildstartdaysbefore = input("Build created when (5minutes,10minutes,1d):")
     if repo_name not in ("release", "master", "prod"):
         print("Wrong input")
@@ -37,7 +31,6 @@ def construct_query(repo_name, lastbuildstartdaysbefore):
 
 
 def fetch_files(data):
-    global display
 
     with open(r'configs/artif_config.json', 'r') as fd:
         config = json.load(fd)
@@ -50,18 +43,13 @@ def fetch_files(data):
             for my_list_index in range(int(response_list)):
                 print(response_json['results'][my_list_index]['name'], " ", response_json['results'][my_list_index]['created'], " ", response_json['results'][my_list_index]['actual_sha1'])
             print("-----------------" * 2)
-            display += ',' + response_json['results'][my_list_index]['name']
-        else:
-            display += ',None'
 
 
 if __name__ == "__main__":
     headers = {
         'content-type': 'text/plain',
     }
-    display = '/syn   '
-    inp, lastd = get_input_from_user()
-    d = construct_query(inp, lastd)
-    fetch_files(d)
-    print('\n', display, '\n')
-    input("Press  any key to continue...")
+    my_repo, my_sinceday = get_input_from_user()
+    query = construct_query(my_repo, my_sinceday)
+    fetch_files(query)
+    input("Press any key to continue...")
